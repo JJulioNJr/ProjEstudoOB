e755c83006136c5633de03fc937d90fac1b0254c
José Julio do nascimento Junior <zejulio_@hotmail.com>
2022-08-23 20:35:06 -0300

    Adicionar arquivos de projeto.

---
+++ ProjEstudoOB/Pessoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ProjEstudoOB {
+    internal class Pessoa {
+
+        private String nome;
+        private String dataNasc;
+        private int cpf;
+        private int telefone;
+
+         public Pessoa(String nome, String dataNasc, int cpf, int telefone) {
+            this.nome = nome;
+            this.dataNasc = dataNasc;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+
+        public Pessoa() { }
+
+        public void  getNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String setNome(String nome) {
+            return nome;
+        }
+        
+        public void getDateTime(String dataNasc) {
+            this.dataNasc = dataNasc;
+        }
+
+        public String setDatanasc(String dataNasc) {
+            return dataNasc;
+        }
+
+        public void getCpf(int cpf) {
+            this.cpf = cpf;
+        }
+
+        public int setCpf(int cpf) {
+           return cpf;
+        }
+
+        public void getTelefone(int telefone) {
+            this.telefone = telefone;         
+        }
+
+        public int setTelefone(int telefone) {
+            return telefone;
+        }
+
+        public override String ToString() {
+            return "\t\n*** Cadastro *** \n"+
+                   "\nNome: "+nome+
+                   "\nData de Nascimento: "+dataNasc+
+                   "\nCPF: "+cpf+
+                   "\nTelefone: " + telefone;
+        }
+    }
+}
---
+++ ProjEstudoOB/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProjEstudoOB {
+    internal class Program {
+
+        static String NomeP() {
+            String nomePessoa = "";
+            Console.Write("Nome: ");
+            return nomePessoa = Console.ReadLine();
+        }
+
+        static String DateNascP() {
+            String dtnNasc;
+            Console.Write("Data de Nascimento: ");
+            return dtnNasc = Console.ReadLine();
+
+        }
+
+        static int cpfP() {
+            int cpfPessoa;
+            Console.Write("CPF: ");
+            return cpfPessoa = int.Parse(Console.ReadLine());
+        }
+
+        static int telP() {
+            int telPessoa;
+            Console.Write("Telefone: ");
+            return telPessoa = int.Parse(Console.ReadLine());
+            
+        }
+
+        static void Main(string[] args) {
+
+            Pessoa pessoa1 = new Pessoa(NomeP(), DateNascP(), cpfP(), telP());
+            Console.Clear();
+            Pessoa pessoa2 = new Pessoa(NomeP(), DateNascP(), cpfP(), telP());
+            Console.Clear();
+            Pessoa pessoa3 = new Pessoa(NomeP(), DateNascP(), cpfP(), telP());
+            Console.Clear();
+
+            Console.WriteLine(pessoa1.ToString());
+            Console.WriteLine(pessoa2.ToString());
+            Console.WriteLine(pessoa3.ToString());
+
+        }
+    }
+}
